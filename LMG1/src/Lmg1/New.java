35191a1c9d5fa4acbfc6aa8047b3ca860b8a7548
testing.devoms <testing.devoms@LBCOITLELTF7TZ.LandMarkGroup.India>
2024-04-29 23:13:10 +0530

    New code

---
+++ LMG1/src/Lmg1/New.java
@@ -0,0 +1,13 @@
+package Lmg1;
+
+public class New {
+	
+	public void method1()
+	{
+		System.out.println("welcome to landmark");
+	}
+public static void main(String[] args) {
+	New n = new New();
+	n.method1();
+}
+}
